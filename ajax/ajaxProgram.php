b03256adf73d167237738a730b46609362a5e9d9
unknown <devethancraig@gmail.com>
2019-04-15 12:27:23 -0600

    Ajax Program pt2-4

--- ajax/ajaxProgram.php
+++ ajax/ajaxProgram.php
@@ -1,7 +1,25 @@
 <?php 
     if(!empty($_POST['range'])) {
         if($_POST['range'] == 'all') {
-            echo json_encode({'brand' => 'Toyota', 'Model' => 'Yaris iA'});
+            $db = @new MySQLi('localhost', 'root', 'ajax');
+            if(json_encode([false, $db->connect_error]));
         }
+        $sql = 'SELECT `car_id`, `brand`, `model` FROM `car` WHERE `brand` = `Ferrari`' ;
+
+        $result->query($SQL);
+        if($result) {
+            if ($result->num_rows == 0) {
+                echo json_encode([false, 'Sorry no matching row...']);
+            } else {
+                while($result->fetch_assoc()) {
+                    $all_rows[] = $each_row;
+                }
+                echo json_encode([true, $all_rows]);
+            }
+            $result->free()''
+        } else {
+            echo json_encode([false, 'Sorry SQL error...']);
+        }
+        $db->close();
     }
 ?>
