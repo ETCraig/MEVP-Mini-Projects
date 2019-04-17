9558d7c036155d372355084033adb0b3179027b2
unknown <devethancraig@gmail.com>
2019-04-17 12:26:51 -0600

    Simple Vue Webpack App

---
+++ cli-simple/simple-vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Red />
+    <White />
+    <Blue />
+  </div>
+</template>
+
+<script>
+import Red from './components/Red';
+import White from './components/White';
+import Blue from './components/Blue';
+
+export default {
+  components: {
+    Red,
+    White,
+    Blue
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ cli-simple/simple-vue-cli/src/components/Blue.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div {
+        height: 300px;
+        background: blue;
+    }
+</style>
